642fff994615cb151958731315e8f081ba235dd3
readonly-mst <yash.bhatnagar+readonly@metasquaretech.com>
2025-08-20 12:26:34 +0530

    added

---
+++ src/index.js
@@ -0,0 +1,45 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+.then (()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(` server is running at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed", err)
+})
+
+
+
+
+
+
+
+
+
+// async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/ ${DB_NAME}`)
+//         app.on("error", (error)=>{
+//            console.log("ERR: ", error);
+//            throw err
+//         })
+         
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+
+//     }catch (error){
+//         console.log("ERROR: ", error)
+//         throw err
+//     }
+// }
